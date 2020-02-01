8abe76afa330e05ec002be53625065271a16c55e
DevonTheFloor <devonthefloor@free.fr>
2020-02-01 17:26:08 +0100

    toggle ok

---
+++ raf2/js/toglr.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('.btn-lg').click(function(){
+    $('footer').toggle(),
+      $('header').toggle();
+  })
+});
+/*$(function( ){
+  $('.fa-chalkboard-teacher').toogle( );
+});
+let menu = document.getElementsByClassName('.btn-ln');
+let header= document.getElementsByName('header');
+let footer = document.getElementsByName('footer';)
+function(cache)=>{
+  
+}*/
